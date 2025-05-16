27f57537d4842b2ef4308aa3066351bce064618d
Fran Diaz B <frandiazb97@gmail.com>
2025-05-16 17:02:53 -0300

    completed

--- saladejuegos/src/app/componentes/encuesta/encuesta.component.ts
+++ saladejuegos/src/app/componentes/encuesta/encuesta.component.ts
@@ -2,6 +2,7 @@ import { Component, inject, OnInit } from '@angular/core';
 import { SupabaseService } from '../../services/supabase.service';
 import { CommonModule } from '@angular/common';
 import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { minRequiredCheckboxesSelected } from './validators/requiredCheckboxes.validator';
 
 @Component({
   selector: 'app-encuesta',
@@ -13,18 +14,36 @@ export class EncuestaComponent implements OnInit{
 
   supaBase = inject(SupabaseService);
 
-  surveyForm!: FormGroup;
-  flagButton:boolean = false;
+  surveyForm!:FormGroup;
+  gameGenres!:FormGroup;
+  lastPart!:FormGroup;
+  startSurvey:boolean = false;
 
   ngOnInit(): void {
 
-    
     this.surveyForm = new FormGroup({
       name: new FormControl(null, [Validators.pattern('^[a-zA-ZÀ-ÿ ]+$'), Validators.required]),
       surname: new FormControl(null, [Validators.pattern('^[a-zA-ZÀ-ÿ ]+$'), Validators.required]),
       age: new FormControl(null, [Validators.min(18), Validators.max(99) ,Validators.required]),
       phoneNumber: new FormControl(null, [Validators.required, Validators.min(1), Validators.max(9999999999)]),
     });
+
+    this.gameGenres = new FormGroup({
+      shooter: new FormControl(false),
+      sport: new FormControl(false),
+      soulslike: new FormControl(false),
+      puzzle: new FormControl(false),
+      platform: new FormControl(false),
+      simulation: new FormControl(false),
+    }, minRequiredCheckboxesSelected(1));
+
+    this.lastPart = new FormGroup({
+      timePlayed: new FormControl(null, Validators.required),
+      question: new FormControl(null, [Validators.pattern('^[a-zA-ZÀ-ÿ0-9 ]+$'), Validators.required])
+    })
+
+    this.surveyForm.addControl('gameGenres', this.gameGenres);
+    this.surveyForm.addControl('playTime', this.lastPart);
   }
 
   get justName() {
@@ -43,23 +62,45 @@ export class EncuestaComponent implements OnInit{
     return this.surveyForm.get('phoneNumber');
   }
 
-  changeButtonBorder(condition:boolean){
+  get justTimePlayed(){
+    return this.lastPart.get('timePlayed');
+  }
+
+  get justQuestions(){
+    return this.lastPart.get('question');
+  }
+
+  startTheSurvey(){
+
+    this.startSurvey = true;
+  }
+
+  changeForms(condition:string){
 
     const element = document.getElementById('form-first-part');
     const elemnt2 = document.getElementById('form-second-part');
+    const element3 = document.getElementById('form-last-part');
     
-    if(condition){
+    if(condition == 'firstToSecond'){
       element?.classList.add('close-form');
       elemnt2?.classList.add('open-form');
     }
-    else{
-      element?.classList.remove('close-form');
+    else if(condition == 'secondToLast'){
       elemnt2?.classList.remove('open-form');
+      element3?.classList.add('open-form');
+    }
+    else if(condition == 'LastToSecond'){
+      element3?.classList.remove('open-form');
+      elemnt2?.classList.add('open-form');
+    }
+    else if(condition == 'secondToFirst')
+    {
+      elemnt2?.classList.remove('open-form');
+      element?.classList.remove('close-form');
     }
   }
 
   enviarForm() {
-
     console.log(this.surveyForm.value);
   }
 
---
+++ saladejuegos/src/app/componentes/encuesta/validators/requiredCheckboxes.validator.ts
@@ -0,0 +1,15 @@
+
+import { ValidatorFn, FormGroup, FormControl, AbstractControl, ValidationErrors } from "@angular/forms";
+
+export function minRequiredCheckboxesSelected(min: number): ValidatorFn {
+    
+    return (formGroup: AbstractControl): ValidationErrors | null => {
+      
+        const totalSelected = Object.values((formGroup as FormGroup).controls)
+        .map(value => value as FormControl)
+        .filter(control => control.value === true).length;
+  
+      return totalSelected >= min ? null : { minSelected: true };
+    };
+  
+}
